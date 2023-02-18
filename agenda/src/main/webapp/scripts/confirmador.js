1fb3ca9541f45bad51e21ff8c4ac210a6d8feade
HamiltonStefani <88253849+HamiltonStefani@users.noreply.github.com>
2023-02-17 23:22:46 -0300

    Projeto agenda de contato

---
+++ agenda/src/main/webapp/scripts/confirmador.js
@@ -0,0 +1,13 @@
+/**
+ * Confirmacao de exclusao de um contato
+ * @author Hamilton Stefani
+ * @param idcon
+ */
+
+ function confirmar (idcon){
+	 let resposta = confirm("Confirma a exclusão deste contato ?")
+	 if (resposta === true){
+		//alert(idcon)
+		window.location.href = "delete?idcon=" + idcon
+	 }
+ }
